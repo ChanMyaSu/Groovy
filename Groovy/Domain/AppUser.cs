ae50e867e60dd9b6c0b0d28235c67aded8f6147f
CHOO ZHI XUAN ISABELLE 2403321I <2403321I@student.tp.edu.sg>
2026-01-18 22:04:42 +0800

    Magic Playlist

--- Groovy/Domain/AppUser.cs
+++ Groovy/Domain/AppUser.cs
@@ -1,13 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿    using System.ComponentModel.DataAnnotations;
 
-namespace Groovy.Domain
-{
-    public class AppUser : BaseDomainModel
+    namespace Groovy.Domain
     {
-        public int UserId { get; set; }
-        public string? Username { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
+        public class AppUser : BaseDomainModel
+        {
+            public int UserId { get; set; }
+            public string? Username { get; set; }
+            public string? Email { get; set; }
+            public string? Password { get; set; }
 
+        }
     }
-}
