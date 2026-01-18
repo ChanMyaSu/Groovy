915b2665a2cedb2f0dac8b2a6d188b51b2b3e5b8
ChanMyaSu <2403702J@student.tp.edu.sg>
2026-01-18 23:08:14 +0800

    .

--- Groovy/Program.cs
+++ Groovy/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddQuickGridEntityFrameworkAdapter();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
+
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
